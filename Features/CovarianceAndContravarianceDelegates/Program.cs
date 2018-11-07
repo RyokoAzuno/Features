e72ec93da3e58bb925c1361a1b3e27fd4c701e98
RyokoAzuno <ryokoazuno@gmail.com>
2018-11-07 22:21:08 +0300

    New features added

---
+++ Features/CovarianceAndContravarianceDelegates/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+using static System.Console;
+
+namespace CovarianceAndContravarianceDelegates
+{
+    class Base
+    {
+        public string Name { get; set; }
+    }
+    class Derived : Base
+    {
+        public int Age { get; set; }
+    }
+
+    class Program
+    {
+        delegate Base CoDelegate(string name);
+        delegate void ContraDelegate(Derived d);
+        delegate T CoGenDelegate<out T>(string name);
+        delegate void ContraGenDelegate<in T>(T obj);
+
+        static void Main(string[] args)
+        {
+            {
+                WriteLine($"---------------Covariance------------");
+                CoDelegate coDel;
+                coDel = Covariance;
+                Base b = coDel("Mike");
+                WriteLine(b.Name);
+
+                WriteLine($"---------------Contravariance------------");
+                ContraDelegate contraDel;
+                contraDel = Contravariance;
+                Derived d = new Derived { Name = "Ann", Age = 15 };
+                contraDel(d);
+                WriteLine(d.Name);
+            }
+            {
+                WriteLine($"---------------Generic Covariance------------");
+                CoGenDelegate<Base> coGenDel;
+                coGenDel = Covariance;
+                Base b = coGenDel("Steve");
+                WriteLine(b.Name);
+
+                WriteLine($"---------------Generi Contravariance------------");
+                ContraGenDelegate<Derived> contraGenDel;
+                contraGenDel = Contravariance;
+                Derived d = new Derived { Name = "Ann", Age = 15 };
+                contraGenDel(d);
+                WriteLine(d.Name);
+            }
+        }
+
+        static Derived Covariance(string name) => new Derived { Name = $"{name} from Covariance", Age = 10 };
+        static void Contravariance(Base b) => b.Name = $"{b.Name} from Contravariance";
+    }
+}
---
+++ Features/Threads/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+using static System.Console;
+
+namespace Threads
+{
+    class Info
+    {
+        //public int Symbol { get; set; }
+        public int X { get; set; }
+        //public int Y { get; set; }
+        public ConsoleColor Color { get; set; }
+    }
+
+    class Program
+    {
+        static readonly object locker = new object();
+
+        static void Main(string[] args)
+        {
+            List<int> lst = new List<int>();
+            string str = string.Empty;
+            Random rnd = new Random();
+
+            Write($"Enter number of lines(dont duplicate): ");
+            str = ReadLine();
+
+            foreach (var item in str.Split(new char[] { ' ' }))
+                lst.Add(Convert.ToInt32(item));
+
+            foreach (var item in lst)
+            {
+                Info inf = new Info() { Color = (ConsoleColor)rnd.Next(1, 15), X = item };
+                Thread th = new Thread(new ParameterizedThreadStart(PrintSymbol));
+                th.Start(inf);
+            }
+        }
+        static void PrintSymbol(object info)
+        {
+            Random rnd = new Random();
+            Info inf = (Info)info;
+            int l = inf.X;
+            int posx = 0;
+            char ch = (char)1;
+
+            for (int i = 0; i < 50; i++)
+            {
+                lock (locker)
+                {
+                    ForegroundColor = inf.Color;
+                    SetCursorPosition(posx, l);
+                    Write($"{ch}");
+                    posx = posx + 1;
+                }
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
