18a54bde47c34b577f6c5f2686104f003185b798
RyokoAzuno <ryokoazuno@gmail.com>
2019-04-19 01:02:12 +0300

    Add Exceptions

---
+++ Features/Exceptions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                try
+                {
+                    Console.WriteLine("Try block!");
+                    throw new StackOverflowException();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Inner Catch block!");
+                    throw;
+                }
+                finally
+                {
+                    Console.WriteLine("Inner Finally block!");
+                }
+            }
+            catch(StackOverflowException)
+            {
+                Console.WriteLine($"Outer Catch block!");
+            }
+            finally
+            {
+                Console.WriteLine("Outer Finally block!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
