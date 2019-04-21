e4eb3bd85a3af9547ddb857bfc1f36c8deb7ba6b
RyokoAzuno <ryokoazuno@gmail.com>
2019-04-21 13:43:23 +0300

    Add Enumeration

---
+++ Features/Enumeration/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Enumeration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lst = new List<int> { 1, 2, 6, 7, 8, 2 };
+
+            using (IEnumerator<int> iter = lst.GetEnumerator())
+            {
+                while (iter.MoveNext())
+                    Console.WriteLine(iter.Current);
+            }
+            Console.ReadLine();
+        }
+    }
+}
